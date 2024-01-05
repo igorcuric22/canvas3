fdcc3ed85dc5058fbdb9f94f9150fce817a30194
igorcuric22 <igor.curic55@gmail.com>
2024-01-05 17:39:54 +0100

    locale storage

--- src/App.js
+++ src/App.js
@@ -8,19 +8,23 @@ function draw(ctx,location) {
   ctx.fillStyle="red";
   ctx.fillRect(x, y, 50, 50);
   ctx.stroke();
-
-
 }
 
 function App() {
-  const [locations, setLocations] = React.useState([])
-  const canvasRef = React.useRef(null)  
+  const [locations, setLocations] = React.useState(
+    JSON.parse(localStorage.getItem('draw-app')) || [] );
+
+  const canvasRef = React.useRef(null);  
   
   React.useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, window.innerHeight, window.innerWidth);
-    locations.forEach(location => draw(ctx, location));
+    locations.forEach(location => draw(ctx, location))
+  })  
+  
+  React.useEffect(() => {
+    localStorage.setItem('draw-app', JSON.stringify(locations))
   })  
   
   function handleCanvasClick(e) {
@@ -38,8 +42,10 @@ function App() {
   
   return (
     <>
-      <button onClick={handleClear}>Clear</button>
-      <button onClick={handleUndo}>Undo</button>
+      <div className="controls">
+        <button onClick={handleClear}>Clear</button>
+        <button onClick={handleUndo}>Undo</button>
+      </div>
       <canvas
         ref={canvasRef}
         width={window.innerWidth}
